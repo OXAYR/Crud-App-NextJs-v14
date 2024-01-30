3783403cc27e79e11d9a968b7e048e040973abda
OXAYR <130404981+OXAYR@users.noreply.github.com>
2024-01-30 18:17:40 +0500

    Added Get endpoint

---
+++ src/app/todos/route.ts
@@ -0,0 +1,20 @@
+import { todos } from "./todos";
+
+export async function GET() {
+	return Response.json(todos);
+}
+
+export async function POST(request: Request) {
+	const todo = await request.json();
+	const newTodo = {
+		id: todos.length + 1,
+		text: todo.text,
+	};
+	todos.push(newTodo);
+	return new Response(JSON.stringify(newTodo), {
+		headers: {
+			"Content-Type": "application/json",
+		},
+		status: 201,
+	});
+}
---
+++ src/app/todos/todos.ts
@@ -0,0 +1,8 @@
+export const todos = [
+	{
+		id: 1,
+		text: "Static todo",
+	},
+];
+
+// localStorage.setItem("todos", JSON.stringify(Todos));
