9dc128b1ab72dbb9346c06f536bd12e4c28140e0
OXAYR <130404981+OXAYR@users.noreply.github.com>
2024-01-30 18:52:26 +0500

    Added edit endpoint

--- src/app/todos/[id]/route.ts
+++ src/app/todos/[id]/route.ts
@@ -8,3 +8,15 @@ export async function GET(
 	return Response.json(todo);
 }
 
+
+export async function PATCH(
+	request: Request,
+	{ params }: { params: { id: string } }
+) {
+	const body = await request.json();
+	const { text } = body;
+	const { id } = params;
+	const index = parseInt(id);
+	todos.splice(index, 0, { id: index, text });
+}
+
