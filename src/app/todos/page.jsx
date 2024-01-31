285d4cebe3bf2b2ac49a2ca384d862de2794ff78
OXAYR <130404981+OXAYR@users.noreply.github.com>
2024-01-31 13:01:49 +0500

    Everything is working fine

--- src/app/todos/page.jsx
+++ src/app/todos/page.jsx
@@ -7,6 +7,7 @@ import "./styles.css";
 export default function TodosPage() {
 	const [newTodoText, setNewTodoText] = useState("");
 	const [todos, setTodos] = useState([]);
+	const [editingTodoId, setEditingTodoId] = useState(null);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -44,6 +45,20 @@ export default function TodosPage() {
 		}
 	};
 
+	const handleEditTodo = async (id, newText) => {
+		try {
+			await axios.patch(`/todos/api/${id}`, { text: newText });
+			setTodos((prevTodos) =>
+				prevTodos.map((todo) =>
+					todo.id === id ? { ...todo, text: newText } : todo
+				)
+			);
+			setEditingTodoId(null);
+		} catch (error) {
+			console.error("Error editing todo:", error);
+		}
+	};
+
 	return (
 		<>
 			<div className="bg-gray-200 p-4">
@@ -64,22 +79,49 @@ export default function TodosPage() {
 					</button>
 				</div>
 			</div>
-			<div className="mt-8">
+			<div className="mt-8 flex flex-col items-center">
 				<h1 className="text-2xl font-semibold mb-4">Todo List</h1>
 				{todos.length > 0 ? (
-					<ul className="list-disc pl-8">
+					<ul className="list-disc w-2/4">
 						{todos.map((todo) => (
 							<li
 								key={todo.id}
 								className="text-gray-700 flex items-center justify-between"
 							>
-								{todo.text}
-								<button
-									onClick={() => handleDeleteTodo(todo.id)}
-									className="text-red-500"
-								>
-									Delete
-								</button>
+								{editingTodoId === todo.id ? (
+									<>
+										<input
+											type="text"
+											value={newTodoText}
+											onChange={(e) => setNewTodoText(e.target.value)}
+											className="border border-gray-300 p-2 flex-grow"
+										/>
+										<button
+											onClick={() => handleEditTodo(todo.id, newTodoText)}
+											className="bg-green-500 text-white px-2 py-1 mr-4"
+										>
+											Save
+										</button>
+									</>
+								) : (
+									<>
+										{todo.text}
+										<div>
+											<button
+												onClick={() => setEditingTodoId(todo.id)}
+												className="text-blue-500 mx-2"
+											>
+												Edit
+											</button>
+											<button
+												onClick={() => handleDeleteTodo(todo.id)}
+												className="text-red-500"
+											>
+												Delete
+											</button>
+										</div>
+									</>
+								)}
 							</li>
 						))}
 					</ul>
